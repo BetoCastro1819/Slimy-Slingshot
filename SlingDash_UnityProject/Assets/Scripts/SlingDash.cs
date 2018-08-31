42c607fb8fb13dc962a5b71bba42860c8f8ed59e
Beto <betocastro1819@gmail.com>
2018-08-31 00:33:40 -0300

    Aim handle added

--- SlingDash_UnityProject/Assets/Scripts/SlingDash.cs
+++ SlingDash_UnityProject/Assets/Scripts/SlingDash.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class SlingDash : MonoBehaviour
 {
+	public GameObject aimHandle;
 	public float slinghsotForce = 100f;
 
 	Rigidbody2D rb;
+	Vector3 mousePos;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		aimHandle.SetActive(false);
 	}
 	
 	void Update ()
@@ -18,27 +21,45 @@ public class SlingDash : MonoBehaviour
 		if (Input.GetKey(KeyCode.Mouse0))
 			SetDirection();
 
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+			CreateAimingHandle();
+
 		if (Input.GetKeyUp(KeyCode.Mouse0))
 			Slingshot();
-
 	}
 
 	void SetDirection()
 	{
-		Vector3 mousePos = Input.mousePosition;
+		mousePos = Input.mousePosition;
 		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
 		Vector2 dir = new Vector2(
-			mousePos.x - transform.position.x,
-			mousePos.y - transform.position.y
+			mousePos.x - aimHandle.transform.position.x,
+			mousePos.y - aimHandle.transform.position.y
 		);
 
 		rb.velocity = Vector2.zero;
-		transform.up = -dir;
+		aimHandle.transform.up = -dir;
+
+		transform.rotation = aimHandle.transform.rotation;
+
+		// If player collides, it keeps rotating after slingshot release
+		//rb.rotation = 0;
+
+		// Ask how to make slow mo effect non-gittery
+		//Time.timeScale = 0.1f;
 	}
 
 	void Slingshot()
 	{
+		//Time.timeScale = 1f;
 		rb.AddForce(transform.up * slinghsotForce);
+		aimHandle.SetActive(false);
+	}
+
+	void CreateAimingHandle()
+	{
+		aimHandle.transform.position = new Vector2(mousePos.x, mousePos.y);
+		aimHandle.SetActive(true);
 	}
 }
