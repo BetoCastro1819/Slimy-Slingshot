04983940a226efc02b86c503a07899a09bca24cb
Unknown <jquintanamartin@hotmail.com>
2018-09-13 11:46:04 -0300

    Moving enemy

---
+++ SlingDash_UnityProject/Assets/Scripts/Enemies/MovingEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEnemy : Enemy
+{
+    private Vector3 dir = Vector3.right;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += speed * dir * Time.deltaTime;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dir *= -1;
+    }
+}
