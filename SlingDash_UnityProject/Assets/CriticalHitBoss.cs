48ef9c0e6213a283f954b229f1cdb70805ae7b46
Unknown <jquintanamartin@hotmail.com>
2018-10-14 13:53:35 -0300

    Boss dies only if all tentacles died

---
+++ SlingDash_UnityProject/Assets/CriticalHitBoss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriticalHitBoss : MonoBehaviour {
+
+    public List<GameObject> criticalPoints;
+
+    [HideInInspector]
+    public int criticalPointsQuant;
+    bool isdone = false;
+
+    private void Start()
+    {
+        criticalPointsQuant = criticalPoints.Count;
+    }
+
+    private void Update()
+    {
+        if (criticalPointsQuant <= 0 && !isdone)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Boss");
+            isdone = true;
+        }
+    }
+}
---
+++ SlingDash_UnityProject/Assets/CriticalPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriticalPoint : Enemy {
+
+    public CriticalHitBoss myBoss;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            myBoss.criticalPointsQuant--;
+            Destroy(gameObject);
+        }    
+    }
+}
