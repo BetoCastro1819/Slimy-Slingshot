12df5de93126ea68121c4779fa9efb9dccdf35d6
Beto <betocastro1819@gmail.com>
2019-12-07 01:35:41 -0300

    MissionUI text is updated based on mission reference

--- SlingDash_UnityProject/Assets/Missions/Mission.cs
+++ SlingDash_UnityProject/Assets/Missions/Mission.cs
@@ -3,6 +3,7 @@
 public abstract class Mission : MonoBehaviour
 {
 	public string missionID;
+	[TextArea]
 	public string missionDescription;
     public bool isComplete;
     public int coinReward;
--- SlingDash_UnityProject/Assets/Scripts/UI/Mission_UI.cs
+++ SlingDash_UnityProject/Assets/Scripts/UI/Mission_UI.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Mission_UI : MonoBehaviour 
 {
 	[SerializeField] Mission mission;
 	[SerializeField] GameObject crossline;
+
+	private void Start() 
+	{
+		Text missionDescription = GetComponent<Text>();
+		missionDescription.text = "- " + mission.missionDescription;	
+	}
 
 	public void CrossMissionIfCompleted() 
 	{
