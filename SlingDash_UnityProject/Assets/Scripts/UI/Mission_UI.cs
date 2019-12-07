5cf294de5a03cc0201fa8276f359ace6388ad26c
Beto <betocastro1819@gmail.com>
2019-12-07 02:09:35 -0300

    Implemented animation for crossing out completed missions

--- SlingDash_UnityProject/Assets/Scripts/UI/Mission_UI.cs
+++ SlingDash_UnityProject/Assets/Scripts/UI/Mission_UI.cs
@@ -5,11 +5,18 @@ public class Mission_UI : MonoBehaviour
 {
 	[SerializeField] Mission mission;
 	[SerializeField] GameObject crossline;
+	[SerializeField] float crosslineMaxScaleX;
+	[SerializeField] float crossAnimationSpeed;
+
+	private bool shouldAnimate;
 
 	private void Start() 
 	{
 		Text missionDescription = GetComponent<Text>();
 		missionDescription.text = "- " + mission.missionDescription;	
+		shouldAnimate = false;
+
+		crossline.transform.localScale = new Vector2(0, crossline.transform.localScale.y);
 	}
 
 	public void CrossMissionIfCompleted() 
@@ -18,5 +25,20 @@ public class Mission_UI : MonoBehaviour
 		bool currentMissionState = PersistentGameData.Instance.gameData.levelsData[levelID].missions[mission.missionID];
 
 		crossline.SetActive(currentMissionState);
+
+		shouldAnimate = currentMissionState;
+	}
+
+	private void Update()
+	{
+		if (!shouldAnimate) return;
+
+		crossline.transform.localScale = new Vector2(
+			crossline.transform.localScale.x + crossAnimationSpeed,
+			crossline.transform.localScale.y
+		);
+
+		if (crossline.transform.localScale.x >= crosslineMaxScaleX)
+			shouldAnimate = false;
 	}
 }
