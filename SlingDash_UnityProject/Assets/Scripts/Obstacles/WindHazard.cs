fb0c0f2edd2cef1918b255c1cbe3a80285f099e4
Beto <betocastro1819@gmail.com>
2019-12-08 21:41:29 -0300

    Added wind hazard

---
+++ SlingDash_UnityProject/Assets/Scripts/Obstacles/WindHazard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class WindHazard : MonoBehaviour 
+{
+	[SerializeField] float windForce;
+
+	private void OnTriggerStay2D(Collider2D other) 
+	{
+		Rigidbody2D rigidbody2D = other.GetComponent<Rigidbody2D>();
+		if (rigidbody2D)
+			rigidbody2D.AddForce(-transform.up * windForce);
+	}
+}
