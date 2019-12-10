4b33737dd366bddb3257698f582a5f2febe23dae
Beto <betocastro1819@gmail.com>
2019-12-09 23:31:01 -0300

    Can purchase items and add them to the inventory

---
+++ SlingDash_UnityProject/Assets/Scripts/UI/InventoryItemButton_UI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class InventoryItemButton_UI : MonoBehaviour 
+{
+	[SerializeField] GameObject itemEquippedMessage;
+	[SerializeField] string itemPath;
+
+	void Start()
+	{
+		UpdateItemEquippedState();
+	}
+
+	public void EquipItem()
+	{
+		PersistentGameData.Instance.SetTrailAsCurrent(itemPath);
+		UpdateItemEquippedState();
+	}
+
+	private void UpdateItemEquippedState()
+	{
+		bool itemIsEquipped = (PersistentGameData.Instance.gameData.currentPlayerTrail == itemPath);
+		if (itemIsEquipped)
+			itemEquippedMessage.SetActive(true);
+		else
+			itemEquippedMessage.SetActive(false);
+	}
+}
--- SlingDash_UnityProject/Assets/Scripts/UI/StoreItemButton_UI.cs
+++ SlingDash_UnityProject/Assets/Scripts/UI/StoreItemButton_UI.cs
@@ -3,13 +3,16 @@ using UnityEngine.UI;
 
 public class StoreItemButton_UI : MonoBehaviour 
 {
+	[Header("Inventory items variables")]
+	[SerializeField] GameObject invetoryItemsParent;
+	[SerializeField] GameObject invetoryItemButtonPrefab;
+
 	[Header("Item variables")]
 	[SerializeField] string itemPath;
 	[SerializeField] int price;
 
 	[Header("UI elements")]
 	[SerializeField] Text priceText;
-	[SerializeField] Text itemAlreadyOwnedMessage;
 
 	void Start()
 	{
@@ -23,6 +26,13 @@ public class StoreItemButton_UI : MonoBehaviour
 	{
 		PersistentGameData.Instance.SubstractFromCoins(price);
 		PersistentGameData.Instance.SetTrailAsCurrent(itemPath);
+
+		GameObject inventoryItem = Instantiate(invetoryItemButtonPrefab);
+		inventoryItem.name = invetoryItemButtonPrefab.name;
+		inventoryItem.transform.parent = invetoryItemsParent.transform;
+		inventoryItem.transform.localScale = Vector3.one;
+
+		Destroy(gameObject);
 
 		// Set item as owned
 		// Add item to player inventory list
