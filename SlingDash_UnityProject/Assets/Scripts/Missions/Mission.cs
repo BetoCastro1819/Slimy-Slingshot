808c5fbf3768aa1ae6f19e83ae8dd673021c4770
Beto <betocastro1819@gmail.com>
2020-02-14 21:47:42 -0300

    Project prefabs folder clean-up

---
+++ SlingDash_UnityProject/Assets/Scripts/Missions/Mission.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class Mission : MonoBehaviour
+{
+	public string missionID;
+	[TextArea]
+	public string missionDescription;
+    public bool isComplete;
+    public int coinReward;
+
+    public abstract bool IsComplete();
+}
